8484b3ae63a7bee06f92be69478a4c5d5e564a71
countowm <countowm@gmail.com>
2022-08-03 14:30:55 +0300

    as

--- Egz-main/main.js
+++ Egz-main/main.js
@@ -1,14 +1,34 @@
-let melynas = document.getElementById("Pavadinimas");
-let Ingredientai = document.getElementById("Ingredientai");
-let Nurodymai = document.getElementById("Nurodymai");
-console.log(melynas);
-console.log(Ingredientai);
-console.log(Nurodymai);
+window.onload = function() {
+    let mygtukas = document.getElementById("skaiciavimai");
+    let melynas = document.getElementById("Pavadinimas");
+    let Ingredientai = document.getElementById("Ingredientai");
+    let Nurodymai = document.getElementById("Nurodymai");
+    let zalias = document.getElementById("input.Pavadinimas");
+    console.log(melynas);
+    console.log(Ingredientai);
+    console.log(Nurodymai);
+    console.log(mygtukas);
+    
+    mygtukas.onclick = function() {
+        alert("mygtukas buvo paspaustas");
+        console.log(Nurodymai.value);
+        let lettersCount = Nurodymai.value.indexOf("a")
+        alert("yra isviso a raidziu: " + (lettersCount + 1));
+        tuscias(melynas, Ingredientai, Nurodymai);
+        pakeiskSpalva(zalias);
+    }
 
-
-function mygtukoPaspaudimas() {
-    alert("mygtukas buvo paspaustas");
-    console.log(Nurodymai.value);
-    let lettersCount = Nurodymai.value.indexOf("a")
-    console.log(lettersCount);
+}
+function tuscias(melynas, Ingredientai, Nurodymai){
+    tekstas1 = melynas.value;
+    tekstas2 = Ingredientai.value;
+    tekstas3 = Nurodymai.value;
+    if (tekstas1  === 0 || tekstas1.length === 0 || tekstas2  === 0 || tekstas2.length === 0 || tekstas3  === 0 || tekstas3.length === 0){
+        alert("tuscia");
+    } else{
+    alert("yra");
+    }
+}
+function pakeiskSpalva(zalias){
+    document.body.style.color = 'green';
 }
